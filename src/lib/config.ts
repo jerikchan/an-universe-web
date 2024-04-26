c0dc9ce6f78dc1fd3aad6d0c3b0021d1f066f938
jerikchan <jerikchan@gmail.com>
2024-04-26 21:57:19 +0700

    feat: faucet

---
+++ src/lib/config.ts
@@ -0,0 +1,16 @@
+import exp from "constants";
+
+// opBNBmainnet
+const opBNBmainnet = {
+  chainId: 56,
+  chainName: 'Binance Smart Chain Mainnet',
+  explorer: 'https://bscscan.com/',
+};
+// openBNBtestnet
+const opBNBtestnet = {
+  chainId: 97,
+  chainName: 'Binance Smart Chain Testnet',
+  explorer: 'https://testnet.bscscan.com/',
+};
+
+export const config = import.meta.env.
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+// import env 
+declare interface ImportMetaEnv {
+  VITE_APP_TITLE: string;
+  VITE_APP_VERSION: string;
+  VITE_API_URL: string;
+  VITE_RECAPTCHA_SITE_KEY: string;
+  VITE_RECAPTCHA_SECRET_KEY: string;
+  VITE_CHAIN_ID: string;
+  VITE_CHAIN_NAME: string;
+  VITE_EXPLORER: string;
+}
